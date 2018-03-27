527115ea3c0dd2d025151735f137c199e3d8423a
wkai666 <vagrant@homestead>
2018-03-27 11:46:15 +0000

    用通用视图构建静态页面

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
