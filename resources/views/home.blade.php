d74e52f9ae837991bfe25c1b9607b3347de4d996
wkai666 <vagrant@homestead>
2018-03-27 12:48:16 +0000

    局部视图以及视图中链接的处理

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.default')
 @section('title', '主页')
 @section('content')
-	<h1>主页</h1>
+	<!-- <h1>主页</h1> -->
+	<div class="jumbotron">
+		<h1>welcome to laravel</h1>
+		<p class="lead">
+			你现在看到的是： <a href="#">哈哈哈</a>
+		</p>
+		<p>一切，将从这里开始</p>
+		<p><a href="{{ route('signup') }}" class="btn btn-lg btn-success">现在注册</a></p>
+	</div>
 @stop
