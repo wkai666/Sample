7b33002dc7115beded67aa10e5b2ed002b15824d
wkai666 <wangkai_2015@126.com>
2018-03-28 08:39:53 +0000

    完成用户登录退出功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -28,6 +28,7 @@ class UsersController extends Controller
     		'password' => bcrypt($request->password),
     	]);
 
+        Auth::login('$user');
     	session()->flash('success', '欢迎来到laravel世界');
     	return redirect()->route('users.show', [$user]);
     }
